dcf142c3ce92c7257d8b77c325099dc0dfcc8058
Peter Viegas <peterviegas@outlook.com>
2023-02-08 21:29:17 +0000

    Change W12MForm.tsx - create useState Specie, Planet and number of Beings

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 
 const W12MForm = () => {
+
+	const [speciesName, setspeciesName] = useState("");
+  	const [planetName, setplanetName] = useState("");
+  	const [numberOfBeings, setnumberOfBeings] = useState<number>(0);
+
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
