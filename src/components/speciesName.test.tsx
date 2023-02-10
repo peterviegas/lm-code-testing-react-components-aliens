66a730cca13ec8489ad96317ffe721d445c63120
Peter Viegas <peterviegas@outlook.com>
2023-02-10 23:43:04 +0000

    include test calls handle change in speciesName.test

--- src/components/speciesName.test.tsx
+++ src/components/speciesName.test.tsx
@@ -1,5 +1,6 @@
 import {render, fireEvent, screen} from '@testing-library/react'
 import SpeciesName,  {SpeciesNameProps} from './speciesName';
+import  user  from '@testing-library/user-event';
 
 test('renders a SpeciesName', async () => {
 	const onChangeSpeciesNameImplementation = jest.fn(() => "test");
@@ -13,6 +14,19 @@ test('renders a SpeciesName', async () => {
 	//const divElement = screen.getByText("humans");
 	expect(screen.getByText('Species Name:')).toBeInTheDocument();
 	expect(screen.getByDisplayValue(mockSpeciesName.speciesName)).toBeInTheDocument();
+  });
 
-
+  test('correctly calls handle change', async () => {
+	const onChangeSpeciesNameImplementation = jest.fn(() => "test");
+	const mockSpeciesName: SpeciesNameProps = {
+		id: 'id',
+		type: 'type',
+		speciesName: 'humans',
+		onChangeSpeciesName: onChangeSpeciesNameImplementation
+	}
+	  render(<SpeciesName {...mockSpeciesName} />);
+	  //await user.type(screen.getByLabelText(labelText),"123");
+	  const input = screen.getByRole('textbox');
+	  await user.type(input, 'elephant');
+	  expect(mockSpeciesName.onChangeSpeciesName).toHaveBeenCalledTimes(8);
   });
