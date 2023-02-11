c3504271278d1a05cebbe232575014a6c27f0ca0
Peter Viegas <peterviegas@outlook.com>
2023-02-11 23:58:56 +0000

    Modified component planetName - implementing verification of the amount and type of characters allowed.

--- src/components/planetName.tsx
+++ src/components/planetName.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ErrorMessage from './errorMessage';
 
 export interface PlanetNameProps{
 	id: string;
@@ -10,12 +11,28 @@ export interface PlanetNameProps{
 const PlanetName : React.FC<PlanetNameProps> = ( {
 	id, type, planetName, onChangePlanetName}) => {
 
+	const [ errorMessage, setErrorMessage ] = useState<string | undefined>();
+
+	const validate: (value: string) => string | undefined = (value) => {
+		if (value.length < 2 || value.length > 49) {
+			return "Planet Name: Must be between 2 and 49 characters."
+		}
+		if (/[^a-z A-Z0-9]/.test(value)) {
+			return "Planet Name: Numbers are allowed, but no special characters."
+		}
+		return undefined;
+	}
+
 	return (
 		<>
         <label htmlFor={planetName}>Planet Name:</label>
 		<input id={id} type={type} defaultValue={planetName} onChange={(e) => {
+			const errorMessage = validate(e.target.value);
+			setErrorMessage(errorMessage);
 			onChangePlanetName(e.target.value);
-		  }} />
+		  }} 
+		  />
+		  {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
 		  </>
     )
 }
