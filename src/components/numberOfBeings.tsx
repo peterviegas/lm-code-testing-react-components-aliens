3cad5a3be93bef91dcec7e1405d65eebeb59b988
Peter Viegas <peterviegas@outlook.com>
2023-02-12 00:15:17 +0000

    Modified component numberOfBeings - implementing verification of the amount and type of characters allowed.

--- src/components/numberOfBeings.tsx
+++ src/components/numberOfBeings.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ErrorMessage from './errorMessage';
 
 export interface NumberOfBeingsProps{
 	id: string;
@@ -10,12 +11,27 @@ export interface NumberOfBeingsProps{
 const NumberOfBeings : React.FC<NumberOfBeingsProps> = ( {
 	id, type, numberOfBeings, onChangeNumberOfBeings}) => {
 
+	const [ errorMessage, setErrorMessage ] = useState<string | undefined>();
+
+	const validate: (value: string) => string | undefined = (value) => {
+		if (parseInt(value)< 1000000000) {
+			return "Number of beings: Must be at least 1,000,000,000."
+		}
+		if (/[^0-9]/.test(value)) {
+			return "Number of beings: Numbers ONLY."
+		}
+		return undefined;
+	}
+
 	return (
 		<>
         <label htmlFor="numberOfBeings">Number of beings:</label>
 		<input id={id} type={type} defaultValue={numberOfBeings} onChange={(e) => {
+			const errorMessage = validate(e.target.value);
+			setErrorMessage(errorMessage);
 			onChangeNumberOfBeings(e.target.valueAsNumber);
 		  }} />
+		  {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
 		  </>
     )
 }
