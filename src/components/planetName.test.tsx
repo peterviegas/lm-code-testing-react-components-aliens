11187baf49907f99df510420a8c9100391e51dc9
Peter Viegas <peterviegas@outlook.com>
2023-02-12 09:42:05 +0000

    Modified component planetName - For the second part of the tests, I kept Neil's example using it instead of test, but I kept test in my phase one to remember the two cases - Changing textbox values to test messages - Must be between 2 and 49 characters long. Special characters are not allowed, but unlike Species it can receive numbers.

--- src/components/planetName.test.tsx
+++ src/components/planetName.test.tsx
@@ -1,8 +1,9 @@
-import {render, screen} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import PlanetName,  {PlanetNameProps} from './planetName';
 import  user  from '@testing-library/user-event';
 
-test('renders a SpeciesName', async () => {
+test(`renders a PlanetName
+	Checks if there is the phrase Planet Name: in the label line`, async () => {
 	const onChangePlanetNameImplementation = jest.fn(() => "test");
 	const mockPlanetName: PlanetNameProps = {
 		id: 'id',
@@ -16,7 +17,9 @@ test('renders a SpeciesName', async () => {
 	expect(screen.getByDisplayValue(mockPlanetName.planetName)).toBeInTheDocument();
   });
 
-  test('correctly calls handle change', async () => {
+  test(`renders a PlanetName,
+	correctly calls handle change,
+	change the value of the textbox and check if the change took place`, async () => {
 	const onChangePlanetNameImplementation = jest.fn(() => "test");
 	const mockPlanetName: PlanetNameProps = {
 		id: 'id',
@@ -30,3 +33,87 @@ test('renders a SpeciesName', async () => {
 	  await user.type(input, 'elephant');
 	  expect(mockPlanetName.onChangePlanetName).toHaveBeenCalledTimes(8);
   });
+
+  //For the second part of the tests, I kept Neil's example using it instead of test, 
+  //but I kept test in my phase one to remember the two cases
+  it(`Given the required props,
+		When the component PlanetName is rendered,
+		Then the appointment description should be present`, () => {
+		const onChangePlanetNameImplementation = jest.fn();
+		const mockPlanetName : PlanetNameProps = {
+		id: 'id',
+		type: 'type',
+		planetName: 'Planet Earth',
+		onChangePlanetName:onChangePlanetNameImplementation
+		};
+
+    	render(<PlanetName {...mockPlanetName} />);
+
+    	expect(
+    		screen.getByText('Planet Name:')
+    	).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component PlanetName is rendered,
+		change the value of the textbox to less than three characters and check the error message in the component`, () => {
+			const onChangePlanetNameImplementation = jest.fn();
+			const mockPlanetName : PlanetNameProps = {
+			id: 'id',
+			type: 'type',
+			planetName: 'Planet Earth',
+			onChangePlanetName:onChangePlanetNameImplementation
+			};
+	
+		render(<PlanetName {...mockPlanetName} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 'p' } });
+
+		const message = 'Planet Name: Must be between 2 and 49 characters.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component PlanetName is rendered,
+		change the textbox value to greater than 23 characters and check the error message in the component`, () => {
+		const onChangePlanetNameImplementation = jest.fn();
+		const mockPlanetName : PlanetNameProps = {
+		id: 'id',
+		type: 'type',
+		planetName: 'Planet Earth',
+		onChangePlanetName:onChangePlanetNameImplementation
+		};
+	
+		render(<PlanetName {...mockPlanetName} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 'iroejgioejgioregioergioerjgioregioregoejrgiorejgiorejgioegjierogejogio' } });
+
+		const message = 'Planet Name: Must be between 2 and 49 characters.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component PlanetName is rendered,
+		change the textbox value character special
+		check the error message in the component`, () => {
+		const onChangePlanetNameImplementation = jest.fn();
+		const mockPlanetName : PlanetNameProps = {
+		id: 'id',
+		type: 'type',
+		planetName: 'Planet Earth',
+		onChangePlanetName:onChangePlanetNameImplementation
+		};
+
+		render(<PlanetName {...mockPlanetName} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 'action = ação.' } });
+
+		const message = 'Planet Name: Numbers are allowed, but no special characters.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
