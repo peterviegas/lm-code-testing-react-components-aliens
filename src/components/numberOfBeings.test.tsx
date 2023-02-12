3787811106002968bcdb73e9664ac3d19084a3ed
Peter Viegas <peterviegas@outlook.com>
2023-02-12 10:12:13 +0000

    Modified component numberOfBeings.test - For the second part of the tests, I kept Neil's example using it instead of test, but I kept test in my phase one to remember the two cases - Changes the value to less than 1,000,000,000 to demonstrate the error message and checks if the field is numeric passing special characters and letters.

--- src/components/numberOfBeings.test.tsx
+++ src/components/numberOfBeings.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import NumberOfBeings,  {NumberOfBeingsProps} from './numberOfBeings';
 import  user  from '@testing-library/user-event';
 
@@ -45,3 +45,105 @@ test(`renders a NumberOfBeings,
 	  const input = screen.getByDisplayValue(1000000);
       expect(input).toBeInTheDocument();
   });
+
+  //For the second part of the tests, I kept Neil's example using it instead of test, 
+  //but I kept test in my phase one to remember the two cases
+  it(`Given the required props,
+		When the component NumberOfBeings is rendered,
+		Then the appointment description should be present`, () => {
+		const onChangeSpeciesNameImplementation = jest.fn();
+		const mocknumberOfBeings: NumberOfBeingsProps = {
+			id: 'id',
+			type: 'type',
+			numberOfBeings: 1000000,
+			onChangeNumberOfBeings: onChangeSpeciesNameImplementation
+		}
+		render(<NumberOfBeings {...mocknumberOfBeings} />);
+
+    	expect(
+    		screen.getByText('Number of beings:')
+    	).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component NumberOfBeings is rendered,
+		change the value of the textbox to less than 1,000,000,000 and check the error message in the component`, () => {
+		const onChangeSpeciesNameImplementation = jest.fn();
+		const mocknumberOfBeings: NumberOfBeingsProps = {
+			id: 'id',
+			type: 'type',
+			numberOfBeings: 1000000,
+			onChangeNumberOfBeings: onChangeSpeciesNameImplementation
+		}
+		render(<NumberOfBeings {...mocknumberOfBeings} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 2 } });
+
+		const message = 'Number of beings: Must be at least 1,000,000,000.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component NumberOfBeings is rendered,
+		change the textbox value to less than 1,000,000,000 and check the error message in the component`, () => {
+		const onChangeSpeciesNameImplementation = jest.fn();
+		const mocknumberOfBeings: NumberOfBeingsProps = {
+			id: 'id',
+			type: 'type',
+			numberOfBeings: 1000000,
+			onChangeNumberOfBeings: onChangeSpeciesNameImplementation
+		}
+		render(<NumberOfBeings {...mocknumberOfBeings} />);
+
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 999999999 } });
+
+		const message = 'Number of beings: Must be at least 1,000,000,000.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component NumberOfBeings is rendered,
+		change the textbox value character special
+		check the error message in the component`, () => {
+		const onChangeSpeciesNameImplementation = jest.fn();
+		const mocknumberOfBeings: NumberOfBeingsProps = {
+			id: 'id',
+			type: 'type',
+			numberOfBeings: 1000000,
+			onChangeNumberOfBeings: onChangeSpeciesNameImplementation
+		}
+		render(<NumberOfBeings {...mocknumberOfBeings} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 'action = ação.' } });
+
+		const message = 'Number of beings: Numbers ONLY.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
+
+	it(`Given the required props,
+		When the component NumberOfBeings is rendered,
+		change the textbox mixed values numbers and letters
+		check the error message in the component`, () => {
+		const onChangeSpeciesNameImplementation = jest.fn();
+		const mocknumberOfBeings: NumberOfBeingsProps = {
+			id: 'id',
+			type: 'type',
+			numberOfBeings: 1000000,
+			onChangeNumberOfBeings: onChangeSpeciesNameImplementation
+		}
+		render(<NumberOfBeings {...mocknumberOfBeings} />);
+		const input = screen.getByRole('textbox');
+		fireEvent.change(input, { target: { value: 'Species 123' } });
+
+		const message = 'Number of beings: Numbers ONLY.';
+    	expect(
+			screen.getByText(new RegExp(message, "i"))
+		).toBeInTheDocument();
+    });
