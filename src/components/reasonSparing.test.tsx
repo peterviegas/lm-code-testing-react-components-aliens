c755db505d0566c6d9c9a6a51620cd79d844150d
Peter Viegas <peterviegas@outlook.com>
2023-02-12 11:27:09 +0000

    Modified comoponent reasonSparing.test - For the second part of the tests, I kept Neil's example using it instead of test, but I kept test in my phase one to remember the two cases - Forcing tests passing 16 characters and 154 characters and checking error messages.

--- src/components/reasonSparing.test.tsx
+++ src/components/reasonSparing.test.tsx
@@ -1,8 +1,9 @@
-import {render, screen} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import ReasonSparing,  {ReasonSparingProps} from './reasonSparing';
 import  user  from '@testing-library/user-event';
 
-test('renders a SpeciesName', async () => {
+test(`renders a ReasonSparing,
+	Checks if there is the phrase Reason for sparing: in the label line`, async () => {
 	const onChangeReasonSparingImplementation = jest.fn(() => "test");
 	const mockReasonSparing: ReasonSparingProps = {
 		id: 'id',
@@ -13,9 +14,70 @@ test('renders a SpeciesName', async () => {
 	render(<ReasonSparing {...mockReasonSparing} />);
 	expect(screen.getByText('Reason for sparing:')).toBeInTheDocument();
 	expect(screen.getByDisplayValue(mockReasonSparing.reasonSparing)).toBeInTheDocument();
-  });
+});
 
-  test('correctly calls handle change', async () => {
+test(`renders a ReasonSparing,,
+correctly calls handle change,
+change the value of the textbox and check if the change took place`, async () => {
+const onChangeReasonSparingImplementation = jest.fn(() => "test");
+const mockReasonSparing: ReasonSparingProps = {
+	id: 'id',
+	type: 'type',
+	reasonSparing: 'Reason for sparing',
+	onChangeReasonSparing:onChangeReasonSparingImplementation
+	}
+	render(<ReasonSparing {...mockReasonSparing} />);
+
+	const input = screen.getByRole('textbox');
+	await user.type(input, 'elephant');
+	expect(mockReasonSparing.onChangeReasonSparing).toHaveBeenCalledTimes(8);
+});
+
+//For the second part of the tests, I kept Neil's example using it instead of test, 
+//but I kept test in my phase one to remember the two cases
+it(`Given the required props,
+	When the component ReasonSparing is rendered,
+	Then the appointment description should be present`, () => {
+	const onChangeReasonSparingImplementation = jest.fn();
+	const mockReasonSparing: ReasonSparingProps = {
+		id: 'id',
+		type: 'type',
+		reasonSparing: 'Reason for sparing',
+		onChangeReasonSparing:onChangeReasonSparingImplementation
+	}
+	render(<ReasonSparing {...mockReasonSparing} />);
+
+	expect(
+		screen.getByText('Reason for sparing:')
+	).toBeInTheDocument();
+});
+
+it(`Given the required props,
+	When the component ReasonSparing is rendered,
+	change the value of the textbox putting 16 letters and check the error message in the component`, () => {
+	const onChangeReasonSparingImplementation = jest.fn();
+	const mockReasonSparing: ReasonSparingProps = {
+		id: 'id',
+		type: 'type',
+		reasonSparing: 'Reason for sparing',
+		onChangeReasonSparing:onChangeReasonSparingImplementation
+	}
+	render(<ReasonSparing {...mockReasonSparing} />);
+
+	const input = screen.getByRole('textbox');
+	fireEvent.change(input, { target: { value: 'abcdefghijklmnop' } });
+
+	const message = 'Reason for sparing: Must be between 17 and 153 characters.';
+	expect(
+		//screen.getByText('ERROR')
+		screen.getByText(new RegExp(message, "i"))
+	//).not.toBeInTheDocument();
+	).toBeInTheDocument();
+});
+
+it(`Given the required props,
+	When the component ReasonSparing is rendered,
+	change the textbox value putting 154 letters and check the error message in the component`, () => {
 	const onChangeReasonSparingImplementation = jest.fn(() => "test");
 	const mockReasonSparing: ReasonSparingProps = {
 		id: 'id',
@@ -23,9 +85,15 @@ test('renders a SpeciesName', async () => {
 		reasonSparing: 'Reason for sparing',
 		onChangeReasonSparing:onChangeReasonSparingImplementation
 	}
-	  render(<ReasonSparing {...mockReasonSparing} />);
-	  //await user.type(screen.getByLabelText(labelText),"123");
-	  const input = screen.getByRole('textbox');
-	  await user.type(input, 'elephant');
-	  expect(mockReasonSparing.onChangeReasonSparing).toHaveBeenCalledTimes(8);
-  });
+	render(<ReasonSparing {...mockReasonSparing} />);
+
+	const input = screen.getByRole('textbox');
+	fireEvent.change(input, { target: { value: 'iroejgioejgioregioergioerjgioregioregoejrgiorejgiorejgioegjierogejogioiroejgioejgioregioergioerjgioregioregoejrgiorejgiorejgioegjierogejogioaaaaaaaaaaaaaa' } });
+
+	const message = 'Reason for sparing: Must be between 17 and 153 characters.';
+	expect(
+		//screen.getByText('ERROR')
+		screen.getByText(new RegExp(message, "i"))
+	//).not.toBeInTheDocument();
+	).toBeInTheDocument();
+});
