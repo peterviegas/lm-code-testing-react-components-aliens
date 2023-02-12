8cc9747cea5ae78fe5d51bc0538e934ec92387bc
Peter Viegas <peterviegas@outlook.com>
2023-02-12 07:17:16 +0000

    Modified component selectTagTwoPlusTwo - implementing the check of the select, being a correct option and an incorrect one.

--- src/components/selectTagTwoPlusTwo.tsx
+++ src/components/selectTagTwoPlusTwo.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ErrorMessage from './errorMessage';
 
 export interface SelectTagTwoPlusTwoProps{
 	id: string;
@@ -9,17 +10,29 @@ export interface SelectTagTwoPlusTwoProps{
 const SelectTagTwoPlusTwo : React.FC<SelectTagTwoPlusTwoProps> = ( {
 	id, selectTagTwoPlusTwo, onChangeSelectTagTwoPlusTwo}) => {
 
+	const [ errorMessage, setErrorMessage ] = useState<string | undefined>();
+
+	const validate: (value: string) => string | undefined = (value) => {
+		if (value !== '4') {
+			return "Error - In what is 2 + 2: 4 must be selected."
+		}
+		return undefined;
+	}
+
 	return (
 		<>
         <label htmlFor={selectTagTwoPlusTwo}>What is 2 + 2?</label>
 		<select 
 			id={id}
 			value={selectTagTwoPlusTwo} onChange={(e) => {
+				const errorMessage = validate(e.target.value);
+				setErrorMessage(errorMessage);
 				onChangeSelectTagTwoPlusTwo(e.target.value);
 			  }}>
 			<option value="4">4</option>
           	<option value="Not 4">Not 4</option>
 		</select>
+		  {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
 		</>
     )
 }
