33a0a7f358d1033b4adfe3d81763528934eb4f3d
Peter Viegas <peterviegas@outlook.com>
2023-02-12 07:26:34 +0000

    Modified component reasonSparing - implementing check for amount of typing > 17 and less than 153 characters.

--- src/components/reasonSparing.tsx
+++ src/components/reasonSparing.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ErrorMessage from './errorMessage';
 
 export interface ReasonSparingProps{
 	id: string;
@@ -10,12 +11,24 @@ export interface ReasonSparingProps{
 const ReasonSparing : React.FC<ReasonSparingProps> = ( {
 	id, type, reasonSparing, onChangeReasonSparing}) => {
 
+	const [ errorMessage, setErrorMessage ] = useState<string | undefined>();
+
+	const validate: (value: string) => string | undefined = (value) => {
+		if (value.length < 17 || value.length > 153) {
+			return "Reason for sparing: Must be between 17 and 153 characters."
+		}
+		return undefined;
+	}
+
 	return (
 		<>
         <label htmlFor={reasonSparing}>Reason for sparing:</label>
 		<textarea id={id} defaultValue={reasonSparing} onChange={(e) => {
+			const errorMessage = validate(e.target.value);
+			setErrorMessage(errorMessage);
 			onChangeReasonSparing(e.target.value);
 		  }} />
+		  {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
 		  </>
     )
 }
